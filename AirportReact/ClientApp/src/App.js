d46f5693e08cf7b71aa529953bdd311e3afa335b
JM <jovannym@mail.ru>
2021-04-14 01:56:59 +0300

    FirstReactCommit

--- AirportReact/ClientApp/src/App.js
+++ AirportReact/ClientApp/src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
-import './custom.css'
+import bootstrap from 'bootstrap';
+import style from './App.css';
 
 export default class App extends Component {
-  static displayName = App.name;
+    //static displayName = App.name;
 
-  render () {
-    return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-      </Layout>
-    );
-  }
+    render () {
+        return (
+            <div className={"sec2"}>
+                <div className={"container"}>
+                    <div className={"uplogo"}>
+                        <p>Not sip me sugar in bear</p>
+                        <h1>Choos song</h1>
+                    </div>
+                    <div className={"section"}>
+                        <div className={"threblock clearfix"}>
+                            <div className={"block"}>
+                                <img src={""} alt={"Губки"}/>
+                                <h3>Попробуй<br/>Муа Муа</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, tempore!</p>
+                            </div>
+                            <div className={"block"}>
+                                <img src={""} alt={"Губки"}/>
+                                <h3>Попробуй<br/>Джага Джага</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+                            </div>
+                            <div className={"block"}>
+                                <img src={""} alt={"Губки"}/>
+                                <h3>Попробуй<br/>ММММ ММММ</h3>
+                                <p>Lorem ipsum dolor sit amet.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
